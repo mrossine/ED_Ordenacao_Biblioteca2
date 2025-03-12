8c1d2faa76f0dee9a10fc5e723a42f1b5961d3a8
Matheus Rossine <matheusrrodrigues2001@gmail.com>
2025-03-12 12:30:45 -0300

    Atualização da massa de dados

--- src/view/PrincipalTempoOrdenacao.java
+++ src/view/PrincipalTempoOrdenacao.java
@@ -1,18 +1,15 @@
 package view;
 
-import java.util.Random;
-
 import controller.TempOrdController;
 
 public class PrincipalTempoOrdenacao {
 
 	public static void main(String [] args) {
 		TempOrdController n = new TempOrdController();
-		Random rd = new Random();
 		int [] vetor = new int [100000];
 		
-		for(int i = 0; i < 100000; i++) {
-			vetor[i] = rd.nextInt(150) + 1;
+		for(int i = 0; i < 1500; i++) {
+			vetor[i] = 1499 - i;
 		}
 		
 		n.ordenaBubble(vetor);
