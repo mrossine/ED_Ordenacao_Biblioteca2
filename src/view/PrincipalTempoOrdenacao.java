bf6dfe4e0efee128019ac1d7e0a57124bce97178
Matheus Rossine <matheusrrodrigues2001@gmail.com>
2025-03-13 16:36:53 -0300

    Atualização do tamanho do vetor

--- src/view/PrincipalTempoOrdenacao.java
+++ src/view/PrincipalTempoOrdenacao.java
@@ -6,7 +6,7 @@ public class PrincipalTempoOrdenacao {
 
 	public static void main(String [] args) {
 		TempOrdController n = new TempOrdController();
-		int [] vetor = new int [100000];
+		int [] vetor = new int [1500];
 		
 		for(int i = 0; i < 1500; i++) {
 			vetor[i] = 1499 - i;
